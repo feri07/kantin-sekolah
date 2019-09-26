5134e6f418d7cb310eb4de05706c6a368010b8b7
Dani Zakaria <daniootsutsuki@gmail.com>
2019-09-26 14:09:54 +0700

    Ubah nama

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Dani King Of Alabamasakiya
+                    Tolak UU KPK
                 </div>
 
                 <div class="links">
